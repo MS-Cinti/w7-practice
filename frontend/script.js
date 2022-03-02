bf3ced993a22ab9e04a86d96ea2bd52d52eecda3
MS-Cinti <szabadicintia@gmail.com>
2022-03-02 15:39:39 +0100

    added countrycard

--- frontend/script.js
+++ frontend/script.js
@@ -1,106 +1,61 @@
-// Lépcsőfokok általában a projekteknél:
 
-// get the data
-function Month(name, id, nth, days){
+function Country(name, short, population, flag, continent) {
     this.name = name;
-    this.id = id;
-    this.nth = nth;
-    this.days = days;
+    this.short = short;
+    this.population = population;
+    this.flag = flag;
+    this.continent = continent;
 }
 
-const months = [
-    new Month("January", "jan", 1, 31),
-    new Month("February", "feb", 2, 28),
-    new Month("March", "march", 3, 31),
-    new Month("April", "apr", 4, 30),
-    new Month("May", "may", 5, 31),
-    new Month("June", "jun", 6, 30),
-    new Month("July", "jul", 7, 31),
-    new Month("August", "aug", 8, 31),
-    new Month("September", "sep", 9, 30),
-    new Month("October", "oct", 10, 31),
-    new Month("November", "nov", 11, 30),
-    new Month("December", "dec", 12, 31)
-]
-//console.log(months);
-
-// prepare data - "business logic", pl codewarsos feladatokál 
-
-const monthSection = (id, h1, days) => { //ezek a paraméterek function scopeok
+//Components
+const header = (logo) => {
     return `
-    <section id="${id}">
-        <h1>${h1}</h1>
-        <div class="days">${days}</div>
-    </section>
-    `;
-
-}
-
-const dayCard = (year, month, day) => {
-    return `
-    <div class="card">
-        <time>${year}</time>
-        <time>${month}</time>
-        <time>${day}</time>
-        <button class="card-btn">"Get day name"</button>
-    </div>
+    <header>
+        <a id="logo">${logo}</a>
+        <button></button>
+    </header>
     `
 }
 
-const dayCards = (month, callDayCard) => {
 
-    let toReturn = "";
+const loadEvent = async _ => { 
+//Get data:
+    const countryRes = await fetch("https://restcountries.com/v3.1/all"); 
+    //await: aszinkron művelet, megvárjuk a letöltődést, a fgv nevénél is jelezni kell az alávonás előtt
+    const countryArr = await countryRes.json();
+    //console.log(countryArr[0]);
 
-    for(let i=1; i <= month.days; i++){
-        toReturn += callDayCard(2022, month.nth, i)
+//Process data:
+    //map()-nek callback fgv kell
+    let countries = countryArr.map(function(country) {
+        return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0]);
+    })
+    console.log(countries);
+
+    let countryCard = countries;
+
+    for (const countryCards of countryCard) {
+        root.insertAdjacentHTML("beforeend", `
+            <div id="card">
+                <h1>${countryCard.name.common}</h1>
+                <p>${countryCard.cca3}</p>
+                <p>${countryCard.population}</p>
+                <p>${countryCard.flags.svg}</p>
+                <p>${countryCard.continents[0]}</p>
+            </div>
+        `)
     }
 
-    return toReturn;
+    const rootElement = document.getElementById("root");
+    //rootElement.insertAdjacentHTML("beforeend", header("Countries"));
 }
 
-//console.log(dayCards(months[0], dayCard));
-
-// components = html elements we would like to add to the document later
+window.addEventListener("load", loadEvent);
 
 
-// render = add the components to the document
+//kártyaként megjeleníteni ezt a countryt
+//egy komponensbe mentsem el ezeket 
+//country cardsban egy db div, aminek card az id-je 
+//load eseményen belül adni hozzá a rootelemhez
 
-// a loadevent visszaad egy objectet, _ azt jelenti,hogy nem használjuk a paraméterét, maradhat () is
-const loadEvent = _ => { 
-
-    let content = "";
-
-    for (const month of months) {
-        content += monthSection(month.id, month.name, dayCards(month, dayCard))
-    }
-
-    document.getElementById("root").insertAdjacentHTML("beforeend", content);
-
-    //clickevent:
-/* 
-    function cardButtonClickEvent(event) {
-
-        console.log(event.target.parentElement);
-        event.target.parentElement.classList.toggle("clicked") //toggle- rárakja,leveszi kattintásokkor
-    }
-
-    const cardList = document.querySelectorAll(".card")
-
-    for (const card of cardList) {
-        //console.log(card);
-        card.querySelector("button").addEventListener("click", cardButtonClickEvent)
-    }
-*/
-    function clickEvent(event){
-        //console.log(event.target);
-        if(event.target.classList.contains("card-btn")){
-            console.log("hello click")
-            event.target.innerHTML = "This button was clicked"
-        }
-    }
-
-    document.addEventListener("click", clickEvent);
-}
-
-window.addEventListener("load", loadEvent); //callback fgv., nincs zárójel a végén, argumentumként adjuk meg
-
+//16.15
