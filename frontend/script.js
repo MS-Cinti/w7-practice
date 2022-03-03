c7b7ae85703a862e91816678c6b2546a81e7e6c0
MS-Cinti <szabadicintia@gmail.com>
2022-03-03 16:45:23 +0100

    added .map()

--- frontend/script.js
+++ frontend/script.js
@@ -5,11 +5,11 @@ function loadEvent() {
     let htmlBody = `
     <form>
         <label for="fname">First name:</label><br>
-            <input type="text" id="fname" name="firstname" value=""><br>
+            <input type="text" id="input1" name="input1" value=""><br>
         <label for="lname">Last name:</label><br>
-            <input type="text" id="lname" name="lastname" value=""><br>
+            <input type="text" id="input2" name="input2" value=""><br>
         <label for="age">Age:</label><br>
-            <input type="text" id="age" name="age" value=""><br>
+            <input type="text" id="input3" name="input3" value=""><br>
     </form><br>
     <button>Click</button>
     `
@@ -18,11 +18,25 @@ function loadEvent() {
     //input eseményfigyelő
     const inputList = document.querySelectorAll("input"); 
 
+    console.log(typeof inputList);
+    console.log(Array.isArray(inputList));
+    /* 
     for (const input of inputList) {
         input.addEventListener("input", function (event) {
             console.log(event.target.value);
         })
     }
+    */
+
+    //.map() a for ciklus helyett:
+    //a map egy tömböt ad vissza, nem kell külön még egy tömb, megcsinálja nekünk
+    //áttekinthetőbb map-el dolgozni
+    Array.from(inputList).map(function(input) {   
+        
+        input.addEventListener("input", function (event) {
+            console.log(event.target.value);
+        })
+    })
 }
 
 window.addEventListener("load", loadEvent);
