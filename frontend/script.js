50ff949a71ea42f322b1705ca291f5c81e7b5c39
MS-Cinti <szabadicintia@gmail.com>
2022-03-03 15:12:27 +0100

    added callback function

--- frontend/script.js
+++ frontend/script.js
@@ -1,75 +1,39 @@
 
-function Country(name, short, population, flag, continent) {
-    this.name = name;
-    this.short = short;
-    this.population = population;
-    this.flag = flag;
-    this.continent = continent;
-}
+function inIt(mathFunction) { //paraméterek mindig function scopeok
+    var a = 5;
+    var b = 6;
 
-const menuButton = _ => { //nincs paraméter, így lehet alávonás
-    return `
-    <button id="menuButton">                                                          
-        <svg width="40" height="40">
-            <rect width="20" height="2"/>
-            <rect width="20" height="2"/>
-            <rect width="20" height="2"/>
-        </svg>
-    </button>
-    `;
-}
-
-//Components
-const header = (logo, button) => {
-    return `
-    <header>
-        <a id="logo">${logo}</a>
-        ${button()}
-    </header>
-    `
-}
-
-const countryCard = (name, short, population, flag, continent) => {
-    return`
-    <div id="card">
-        <h1>${name}</h1>
-        <p>${short}</p>
-        <p>population: ${population}</p>
-        <img src="${flag}" alt=""></img>
-        <p>continent: ${continent}</p>
-    </div>
-    `
-}
-
-const loadEvent = async _ => { 
-//Get data:
-    const countryRes = await fetch("https://restcountries.com/v3.1/all"); 
-    //await: aszinkron művelet, megvárjuk a letöltődést, a fgv nevénél is jelezni kell az alávonás előtt
-    const countryArr = await countryRes.json();
-    //console.log(countryArr[0]);
-
-//Process data:
-    //map()-nek callback fgv kell
-    let countries = countryArr.map(function(country) {
-        return new Country(country.name.common, country.cca3, country.population, country.flags.svg, country.continents[0]);
-    })
-    console.log(countries);
-
-    let cards = ""
-
-    for (const country of countries){
-        cards += countryCard(country.name, country.short, country.population, country.flag, country.continent)
-    }
-
-    const root = document.getElementById("root");
     
-    root.insertAdjacentHTML(`beforeend`,header("Countries",menuButton))
-    root.insertAdjacentHTML(`beforeend`,cards)
+    if (a < b){
+        let c = mathFunction(b, a)
+        console.log(c);
+       /*  (function() {
+            var c = b - a;
+            console.log(c);
+        })();   */
+    }else{
+        let c = mathFunction(a, b)
+        console.log(c);
+    /*     (function (){
+            var c = a - b;
+        })(); */
+    };
 
-    const gotMenuButton = document.getElementById("menuButton");
-    gotMenuButton.addEventListener("click", (event) => {
-        event.target.classList.toggle("clicked");
-    });
+    //console.log(c);
+    //console.log(c);
+    //a c látszódik az if után
 }
 
-window.addEventListener("load", loadEvent);
+const inItC = (firstNumber, secondNumber) => {
+    return firstNumber - secondNumber;
+}
+
+const inItD = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
+}
+
+inIt(inItD);
+
+//készítsétek el a szokásos load eseményt új brachbe events néven és csináljatok egy formot, és a formban legyen 3 input mező, be is lehet css elni szöveges input mezők legyenek, egy gomb legyen benne és az input mezőkre mindegyikre input eseményt tegyetek rá, mindegyik inputon legyen input esemény, clgolja ki az értékét az input mezőnek, js ből adjuk hozzá a root id divhez a formot , clg az input eseményre az input mezők értékét
+//15.50
+
